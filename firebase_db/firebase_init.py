2612635b8745747bf75f66167078e2d334f45cb1
Siva-Rajan-R <siva967763@gmail.com>
2025-06-06 19:21:22 +0530

    updated to firebase-admin instead pyfcm

--- firebase_db/firebase_init.py
+++ firebase_db/firebase_init.py
@@ -1,14 +1,7 @@
 import pyrebase
 
 config=firebaseConfig = firebase_config = {
-    "apiKey": "AIzaSyDfQB7WFMOj4xy4efWAWhKTJT2D-3jDPmY",
-    "authDomain": "nanmaitharvuar-kovilapp.firebaseapp.com",
-    "databaseURL": "https://nanmaitharvuar-kovilapp-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "nanmaitharvuar-kovilapp",
-    "storageBucket": "nanmaitharvuar-kovilapp.appspot.com",
-    "messagingSenderId": "424295283811",
-    "appId": "1:424295283811:web:3c5c0ea86136d28f4a4c56",
-    "measurementId": "G-77RXRTCNE6"
+    
 }
 
 firebase_init=pyrebase.initialize_app(config=config)
