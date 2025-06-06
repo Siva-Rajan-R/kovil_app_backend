7d13dc6dd2753d66b22efce488b4b9b5c001eb8f
Siva-Rajan-R <siva967763@gmail.com>
2025-06-06 19:42:25 +0530

    newly commited

--- firebase_db/firebase_init.py
+++ firebase_db/firebase_init.py
@@ -1,9 +1,11 @@
 import pyrebase
+import os,json
+from dotenv import load_dotenv
+load_dotenv()
 
-config=firebaseConfig = firebase_config = {
-    
-}
+config=json.loads(os.getenv("FIREBASE_CONFIG"))
 
+print(type(config))
 firebase_init=pyrebase.initialize_app(config=config)
 
 
