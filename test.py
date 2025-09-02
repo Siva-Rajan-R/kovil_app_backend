ef56108a0703e2a93fdce032de7d0e510657ba63
Siva-Rajan-R <siva967763@gmail.com>
2025-09-02 22:26:26 +0530

    added subprocess

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-import json
+# run_both.py
+import subprocess
 
-msg=firebaseConfig = {
-  'apiKey': "AIzaSyA1_L6mwNYkWCIgBgaJjBaoCAC868Lb9tQ",
-  'authDomain': "guruvudhasan-d77ac.firebaseapp.com",
-  'databaseURL': "https://guruvudhasan-d77ac-default-rtdb.asia-southeast1.firebasedatabase.app",
-  'projectId': "guruvudhasan-d77ac",
-  'storageBucket': "guruvudhasan-d77ac.firebasestorage.app",
-  'messagingSenderId': "70181313330",
-  'appId': "1:70181313330:web:66fc39ceb78931212b152b",
-  'measurementId': "G-D3PFLGHZSZ"
-}
-print(json.dumps(msg))
+# start uvicorn
+uvicorn_proc = subprocess.Popen(["uvicorn", "main:app", "--reload"])
+
+# start arq worker
+arq_proc = subprocess.Popen(["arq", "arq_bgtasks.main.WorkerSettings"])
+
+# wait for both
+uvicorn_proc.wait()
+arq_proc.wait()
+
